6018801b99172752661317ff2b012e78b7742610
Jeison Esquivel <98560347+jeisonrej@users.noreply.github.com>
2022-06-03 18:29:59 -0500

    Ajustes en leyenda del reporte

--- js/normativa.js
+++ js/normativa.js
@@ -8,10 +8,10 @@ var normatividad ={"norma":[
     {"capa":"aerodromos", "descripcion":"No se podrá operar desde un aeródromo o en sus proximidades dentro de un radio de 9 km (4,8 NM) medidos desde el ARP."},
     {"capa":"centrosPobladosPol", "descripcion":" La operación no podrá efectuarse directamente sobre público, reuniones de personas al aire libre, aglomeraciones de edificios, ciudades u otras áreas pobladas o congestionadas."},
     {"capa":"consideracionUno", "descripcion":"https://www.aerocivil.gov.co/normatividad/Resoluciones%20TA%202018/RESL.%20%20N%C2%B0%2004201%20%20DIC%2027%20de%20%202018.pdf"},    
-    {"capa":"consideracionDos", "descripcion":"Recuerde que la UAS debe estar previamente registrada en la base de datos de la  UAEAC y tener la palca de identificacion respectiva."},
+    {"capa":"consideracionDos", "descripcion":"Recuerde que la UAS debe estar previamente registrada en la base de datos de la  UAEAC y tener la placa de identificacion respectiva."},
     {"capa":"consideracionTres", "descripcion":"Cualquier operación aérea que implique trabajos aéreos especiales diferentes de la simple captura de imágenes, fijas o en movimiento, requerirá de permiso especial de la UAEAC."},
     {"capa":"consideracionCuatro", "descripcion":"Todo vuelo con UAS de Clase B, o de Clase A en uso de desviaciones a las limitaciones establecidas en en el párrafo (a) de la sección 2.2. de este Apéndice, estará precedido de una solicitud de autorización para su realización y de la consecuente aprobación por parte de la UAEAC, siempre y cuando se cumplan todos los presupuestos establecidos en este Apéndice"},    
     {"capa":"resume", "descripcion":"Conforme al estudio de prefactibilidad realizado con la información del sistema y el polígono sujeto a consulta, se deben tener en cuenta las siguientes recomendaciones: "},
-    {"capa":"advertencia", "descripcion":"El presente concepto es emitido de acuerdo con en el cotejo de capas geográficas generadas conforme a las restricciones contempladas en la norma 04201 de 2018 RAC 13 y el polígono topográfico allegado por el usuario. Este concepto no constituye una decisión de fondo."},
+    {"capa":"advertencia", "descripcion":"El presente reporte fue generado conforme a las restricciones contempladas en la norma 04201 de 2018 RAC 13, sin embargo, el mismo no constituye un concepto oficial por parte de la Aerocivil, para mayor información remítase a esta entidad."},
     {"capa":"NoPolygon", "descripcion":"Por favor, dibuje o importe un polígono para realizar el concepto de prefactibilidad."}
   ]}
